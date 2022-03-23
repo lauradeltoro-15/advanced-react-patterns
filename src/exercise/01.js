356f71ea617e18f371ecacb907552c09d468c3cf
Laura <laura.deltoro@seddi.com>
2022-03-23 17:38:49 +0000

    Context module pattern

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -64,7 +64,17 @@ function UserProvider({children}) {
   const value = [state, dispatch]
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
+async function updateUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates})
+  try {
+    const updatedUser = await userClient.updateUser(user, updates);
+    dispatch({type: 'finish update', updatedUser})
+  }catch (error) {
+    dispatch({type: 'fail update', error})
+    throw error
+  }
 
+}
 function useUser() {
   const context = React.useContext(UserContext)
   if (context === undefined) {
@@ -73,14 +83,10 @@ function useUser() {
   return context
 }
 
-// 🐨 add a function here called `updateUser`
-// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
-// this function. It should accept: dispatch, user, and updates
-
-// export {UserProvider, useUser}
+// export {UserProvider, useUser, updateUser}
 
 // src/screens/user-profile.js
-// import {UserProvider, useUser} from './context/user-context'
+// import {UserProvider, useUser, updateUser} from './context/user-context'
 function UserSettings() {
   const [{user, status, error}, userDispatch] = useUser()
 
@@ -98,11 +104,7 @@ function UserSettings() {
   function handleSubmit(event) {
     event.preventDefault()
     // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser(userDispatch,user, formState)
   }
 
   return (
